630a1ebb26139fb4b0f7e768a65b3797ac3c35ee
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-08-12 16:18:43 +0100

    add sample data

--- src/data/data.js
+++ src/data/data.js
@@ -1,20 +1,14 @@
 const projects = [
-    { id: 1, name: 'Sample 1' },
-    { id: 2, name: 'Sample 2' },
-    { id: 3, name: 'Sample 3' },
+    { id: 1, name: 'Learn React Native' },
+    { id: 2, name: 'Workout' },
 ];
 
 const tasks = [
-    { id: 1, title: '', completed: true, projectID: 1 },
-    { id: 2, title: '', completed: false, projectID: 1 },
-    { id: 3, title: '', completed: false, projectID: 1 },
-    { id: 4, title: '', completed: false, projectID: 1 },
-    { id: 5, title: '', completed: true, projectID: 2 },
-    { id: 6, title: '', completed: false, projectID: 2 },
-    { id: 7, title: '', completed: false, projectID: 3 },
-    { id: 8, title: '', completed: false, projectID: 3 },
-    { id: 9, title: '', completed: true, projectID: 3 },
-    { id: 10, title: '', completed: false, projectID: 3 }
+    { id: 1, title: 'Install Node', completed: true, projectID: 1 },
+    { id: 2, title: 'Install React Native CLI:', completed: false, projectID: 1 },
+    { id: 3, title: 'Install Xcode', completed: false, projectID: 1 },
+    { id: 4, title: 'Morning Jog', completed: true, projectID: 2 },
+    { id: 5, title: 'Visit the gym', completed: false, projectID: 2 }
 ];
 
 module.exports = { projects, tasks };
